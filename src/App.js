65119793b62b87c653f997b6fff247e68a53f60e
pycodersh <114210236+pycodersh@users.noreply.github.com>
2023-01-13 20:50:43 +0900

    Expenses, Card components Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
+import Expenses from './components/Expenses';
 
 function App() {
-  const expense = [
+  const expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -30,26 +31,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
 }
